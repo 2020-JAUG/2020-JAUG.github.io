55e559340cdcc6e9b7a5c0488a1366f5ee7f3d36
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-23 15:36:59 +0200

    Export files to use RDX

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,4 +1,5 @@
-export const LOGIN = 'login'
-export const LOGOUT = 'logout'
-export const UPDATE_USER = 'update_user'
-export const LEFT_USER = 'left_user'
+export const LOGIN = "login";
+export const LOGOUT = "logout";
+export const UPDATE_USER = "update_user";
+export const ADD_MOVIES = "ADD_MOVIES";
+export const LEFT_USER = "left_user";
