abe712da5686efb8726b89478a23883b966c785d
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-23 22:36:46 +0200

    Import login && profile file on App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Fragment } from 'react'
 import Header from './components/Header/Header';
 import Home from './containers/Home/Home';
 import Register from './containers/Register/Register';
+import Profile from './containers/Profile/Profile';
 import Detail from './containers/Detail/Detail';
 import Login from './containers/login/Login';
 import TopRated from './containers/TopRated/TopRated';
@@ -20,11 +21,12 @@ function App() {
 
             <Switch>
               <Route path="/" exact component={Home}/>
+              <Route path="/login" exact component={Login}/>
+              <Route path="/profile" exact component={Profile}/>
               <Route path="/register" exact component={Register}/>
               <Route path="/upcoming" exact component={UpComing}/>
               <Route path="/toprated" exact component={TopRated}/>
               <Route path="/detail" exact component={Detail}/>
-              <Route path="/login" exact component={Login}/>
             </Switch>
 
           </BrowserRouter>
