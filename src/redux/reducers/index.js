89c8ad9aa9efb670b32591446d509f0819ee5fec
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-23 15:37:22 +0200

    Import credentials and movies to RDX

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
-import credentials from './credentials-reducer';
+import credentials from "./credentials-reducer";
+import movies from "./characters-reducer";
 
-const rootReducer = combineReducers ({
-
-    // aquí importaremos todos los reducers:
-    credentials,
+const rootReducer = combineReducers({
+  // aquí importaremos todos los reducers:
+  credentials,
+  movies,
 });
 
 export default rootReducer;
