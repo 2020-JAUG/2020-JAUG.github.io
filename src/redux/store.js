c2c62ff577ff11b137cbbd59fadd599044641345
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-01 23:59:25 +0200

    Import movies on the store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,13 +3,13 @@ import { save, load } from "redux-localstorage-simple";
 import reducer from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(
-	save({ states: ['credentials', 'cart', 'infoUser'] })
+	save({ states: ['credentials', 'cart', 'infoUser', 'movies'] })
 )(createStore);
 
 //Aqui guardamos en el local-storage de RDX
 const store = createStoreWithMiddleware(
     reducer,
-    load({ states: ['credentials', 'cart', 'infoUser'] }),
+    load({ states: ['credentials', 'cart', 'infoUser', 'movies'] }),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({
         trace: true,
     })
