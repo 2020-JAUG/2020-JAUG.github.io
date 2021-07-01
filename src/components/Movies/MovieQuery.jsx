f07034a139cb59c9a5e39a5b4eb5cc8e447ecd31
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-01 23:46:45 +0200

    Update file to find movie by title

--- src/components/Movies/MovieQuery.jsx
+++ src/components/Movies/MovieQuery.jsx
@@ -1,100 +1,89 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { connect } from 'react-redux';
-import axios from 'axios';
 import { ADD_MOVIES } from '../../redux/types';
-import { useHistory } from 'react-router';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
-
-const MovieQuery = (props) => {
+const MoviesQuery = (props) => {
 
     let history = useHistory();
+    const [movie, setMovie] = useState ({
+        name: "",
+    });
 
-    const [querys, setQuerys] = useState ([]);
-    const [title, setTitle] = useState ([]);
-
-     // Esto es un Handler
-     const updateDatos = (e) => {
-        setQuerys({...querys, [e.target.name]: e.target.value})
-    }
 
     useEffect(() => {
-        setQuerys();
-    }, []);
+        findTitle();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
 
-    const query = async (props) => {
+    // Esto es un Handler
+     const updateDatos = (e) => {
+        setMovie({...movie, [e.target.name]: e.target.value})
+    }
+
+    const clickHandler = (detail) => {
+
+        props.dispatch({ type: ADD_MOVIES, payload: detail });
+        history.push("/detail");
+    };
+
+
+    const findTitle = async () => {
 
         let query = document.getElementById("title").value;
         let body = {
-            query: query
+            query: query,
+            datos: movie.name
         }
 
-        axios
-        .post("http://localhost:3001/movies/search", body)
-        .then((res) => {
-            setTitle(res.data.results);
-            // history.push("/movietitle");
-            console.log('datos', res.data.results);
-            //Guardo en RDX
-            props.dispatch({type:ADD_MOVIES, payload: res.data.results});
-        })
-        .catch((err) => {
-            // console.log(err.response.data.message);
-            return Error("Files not Found");
+          axios
+          .post("http://localhost:3001/movies/search", body)
+          .then((res) => {
+
+              props.dispatch({type:ADD_MOVIES, payload: res.data.results});
+
+              history.push('/moviesgenre');
+          })
+          .catch(() => {
+            return Error("Wrong user or password");
         });
     }
-    const clickHandler = (detail) => {
-        props.dispatch({ type: ADD_MOVIES, payload: detail });
-            // history.push("/movietitle");
-    };
 
-    // if(props.movies?.id) {
+    const baseImgUrl = "https://image.tmdb.org/t/p"
+    const size = "w200"
 
-    //     return (
-
-    //         <div>
-    //             <input className="option" type="text" id="title"  onChange={updateDatos}/>
-    //             <button className="sendButton" name="movie" onClick={() => query()}>Find</button>
-    //         </div>
-    //     )
-    // }
-
-    const baseImgUrl = "https://image.tmdb.org/t/p";
-    const size = "w200";
-
-    if(title[0]?.id) {
-
-        return(
-
-            <div className="movieContent">
-
-            {title.map((movie, index) => (
-
-                <div className="content" onClick={() => clickHandler(movie)}  key={index}>
-                    <div className="content2"  >
-                            <p className="text">{movie.title} </p>
-                            <img src={`${baseImgUrl}/${size}${movie.poster_path}`}  alt="poster"/>
-                            {/* <p className="text">{movie.popularity}</p> */}
-                            {/* <p className="text">{movie.release_date}</p>
-                            {/* <p className="text">{movie.vote_average}</p>
-                            {/* <p className="text">{movie.genre_id}</p>
-                            {/* <p className="text">{movie.overview}</p> */}
-                            {/* <p className="text">{movie.getSimilarMovies}</p>
-                        {/* <div className="enviar" onClick={() => llevame()}></div> */}
-                    </div>
+    if (movie[0]?.id) {
+        return (
+            <div className="allContent">
+                <div className="movieImage">
+                    <div className="fondoIMage"></div>
                 </div>
-            ))}
-        </div>
-    )} else {
-        return(
-            <div>
-               <input className="option" type="text" id="title"  onChange={updateDatos}/>
-               <button className="sendButton" name="movie" onClick={() => query()}>Find</button>
+                <div className="movieContent" >
+                    {props.movies.map((movie, index) => (
+                        <div className="content" key={index} onClick={() => clickHandler(movie)}>
+                            <div className="content2" >
+                                <p className="text">{movie.title} </p>
+                                <img src={`${baseImgUrl}/${size}${movie.poster_path}`} alt="poster"/>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         )
+    } else {
+        return (
+            <div className="spinnerContainer">
+                <div className="spinner">
+                    {/* <img  src={spinner} alt="spinner" width="60" /> */}
+                    <input className="option" type="text" id="title" name="name"  onChange={updateDatos}/>
+                    <button className="sendButton" name="movie" onClick={() => findTitle()}>Find</button>
+                </div>
+            </div>
+        );
     }
-}
-
+};
 export default connect((state) => ({
-    credentials:state.credentials,
-    movies: state.movies,
-}))(MovieQuery);
+    credentials: state.credentials,
+    movies: state.movies
+}))(MoviesQuery);
