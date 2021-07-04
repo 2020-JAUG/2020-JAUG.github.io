d3e1d5668bee1562a4d4ef461439bf5474eae034
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-04 17:50:24 +0200

    Create function to show all order to the Admin

--- src/containers/Admin/AllOrders/AllOrders.jsx
+++ src/containers/Admin/AllOrders/AllOrders.jsx
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import spinner from '../../../assets/spinner2.gif';
 import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import moment  from 'moment';
+import { ORDERS, UPDATE_ORDERS } from '../../../redux/types';
 
 
 const AllOrders = (props) => {
+
+    let history = useHistory();
 
     const [orders, setOrders] = useState([]);
 
@@ -16,7 +20,8 @@ const AllOrders = (props) => {
 
     useEffect( () => {
         findOrders();
-    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
 
     const findOrders = async () => {
 
@@ -26,6 +31,7 @@ const AllOrders = (props) => {
 
             let res = await axios.get("http://localhost:3001/orders",  {headers:{'authorization':'Bearer ' + token}});
             setOrders(res.data);
+            props.dispatch({ type: ORDERS, payload: res.data });
 
         } catch (error) {
             console.log(error);
@@ -33,22 +39,28 @@ const AllOrders = (props) => {
     }
 
     const deleteOrder = async (order) => {
-        console.log(order)
         let token = props.credentials?.token;
         let user = props.credentials?.user;
 
         let body = {
           id: order.id,
-          clinic: order.clinicId
+          user: user.id
         };
 
         console.log(body)
 
-        let res = await axios.post('http://localhost:3001/orders/delete', body, {
-          headers: { authorization: "Bearer " + token }
-        });
+        let res = await axios.post('http://localhost:3001/orders/delete', body, {headers: { authorization: "Bearer " + token }});
+
+        setOrders(res.data);
 
         window.location.reload();
+    }
+
+    const updateOrder = (detail) => {
+
+        props.dispatch({type: ORDERS, payload: detail});
+
+        history.push('/updateorders');
     }
 
     const baseImgUrl = "https://image.tmdb.org/t/p";
@@ -61,7 +73,7 @@ const AllOrders = (props) => {
 
                 <div className="movieContent">
                     {orders.map((order, index) => (
-                        <div className="content" key={index}>
+                        <div className="content" key={index}  >
 
                         <div  className="cardOrder">
                             <p>Rented by:  {order.userName}  {order.lastName}</p>
@@ -70,7 +82,16 @@ const AllOrders = (props) => {
                             <p className="order"> Rental Date : { moment (order.rentalDate).format('LL')} </p>
                             <p className="order"> Return Date : { moment (order.returnDate).format('LL')} </p>
 
-                            <div className="updateButton" onClick={() => deleteOrder(order)}>REMOVE</div>
+                            <div className="buttons">
+                            <div className="updateButton2" onClick={() => deleteOrder(order)} >REMOVE</div>
+                            <div
+                                className="updateButton2"
+                                onClick={() => updateOrder(order)}
+                            >
+                                UPDATE
+                            </div>
+
+                            </div>
                             <img
                             className="imgAdmin"
                             src={`${baseImgUrl}/${size}${order.moviePoster}`}
