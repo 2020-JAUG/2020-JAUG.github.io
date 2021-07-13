d9af3248659334d215da2c6d653e30104ceda021
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-13 17:37:05 +0200

    Change position of the arrows

--- src/containers/MoviesGenre/MoviesGenre.jsx
+++ src/containers/MoviesGenre/MoviesGenre.jsx
@@ -88,8 +88,8 @@ const MoviesGenre = (props) => {
                             </div>
                         </div>
                     ))}
-                    <div Style="color: white;" onClick={()=> changePage("-")}><FontAwesomeIcon icon={faArrowLeft}/></div><div Style="width: 17em;"></div>
-                    <div Style="color: white;" onClick={()=> changePage("+")}><FontAwesomeIcon icon={faArrowRight}/></div>
+                    <div Style="width: 3em;"></div><div className="arrowD" Style="color: white;" onClick={()=> changePage("-")}><FontAwesomeIcon icon={faArrowLeft}/></div><div Style="width: 9em;"></div>
+                    <div Style="width: 6em;"></div><div className="arrowD" Style="color: white;" onClick={()=> changePage("+")}><FontAwesomeIcon icon={faArrowRight}/></div><div Style="width: 3em;"></div>
                 </div>
             </div>
         )
